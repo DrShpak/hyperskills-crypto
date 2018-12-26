be02c052770d32c6f0701c3ea7ce5d9d6f91fc7e
Dmitriy Titov <titov.dmitriy2000@yandex.ru>
2018-12-26 15:14:33 +0300

    2 java files

---
+++ src/test/git/HelloWorld1.java
@@ -0,0 +1,7 @@
+package test.git;
+
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/git/HelloWorld2.java
@@ -0,0 +1,4 @@
+package test.git;
+
+public class HelloWorld2 {
+}
